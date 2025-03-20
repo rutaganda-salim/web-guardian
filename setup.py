1843dc6c0da31d9114ddb9d59e26155e2c4d4251
Salim Rutaganda <rutagandasalim@gmail.com>
2025-03-20 17:21:30 +0200

    fix: small bug fixes

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name                =   "rapidscan",
+    name                =   "web-guardian",
     version             =   '1.2',
     description         =   "The Multi-Tool Web Vulnerability Scanner.",
     long_description    =   README,
     long_description_content_type = "text/markdown",
-    url                 =   "https://github.com/skavngr/rapidscan",
+    url                 =   "https://github.com/rutaganda-salim/web-guardian",
     author              =   "sh4nx0r",
     py_modules          =   ['rapidscan',],
     install_requires    =   [],
